ac0cd36fcc83b8250e0cc19005b65c808c24dadc
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-12 21:11:58 +0200

    main

--- DesignPatternsDemo/Program.cs
+++ DesignPatternsDemo/Program.cs
@@ -10,15 +10,15 @@ namespace DesignPatternsDemo
     {
         static void Main(string[] args)
         {
-            var myCar = new Vehicle("AB-1234");
-            myCar.LocationChanged += MyCarOnLocationChanged;
-            myCar.LocationChanged += PrintCar;
-            myCar.Location = new Location(28, 32);
-            myCar.Location = new Location(28, 32);
-            myCar.Location = new Location(30, 32);
-            
             var fleetManager = new FleetManager(new Location(0, 0));
+            var myCar = new Vehicle("AB-1234");
             fleetManager.AddVehicle(myCar);
+            myCar.LocationChanged += MyCarOnLocationChanged;
+
+            myCar.Location = new Location(10, 10);
+            
+
+            
         }
 
         private static void PrintCar(object sender, EventArgs e)
