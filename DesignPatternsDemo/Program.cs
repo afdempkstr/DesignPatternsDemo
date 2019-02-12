b0bf2942d43cea7b88f88b5edd7b828c06d777d8
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-12 21:44:02 +0200

    other car

--- DesignPatternsDemo/Program.cs
+++ DesignPatternsDemo/Program.cs
@@ -12,16 +12,20 @@ namespace DesignPatternsDemo
         {
             var fleetManager = new FleetManager(new Location(0, 0));
             var myCar = new Vehicle("AB-1234");
+            var myOtherCar = new Vehicle("ZZ-4321");
             fleetManager.AddVehicle(myCar);
+            fleetManager.AddVehicle(myOtherCar);
             myCar.LocationChanged += MyCarOnLocationChanged;
-
+            myOtherCar.LocationChanged += MyCarOnLocationChanged;
             myCar.Location = new Location(10, 10);
+            myOtherCar.Location = new Location(-20, 10);
 
             fleetManager.RemoveVehicle(myCar.Id);
 
-            for (double i = myCar.Location.Latitude; i >= -10; i--)
+            for (int step = 0; step < 50; step++)
             {
-                myCar.Location = new Location(i, i);
+                myCar.Location = new Location(myCar.Location.Latitude - 1, myCar.Location.Longitude -1);
+                myOtherCar.Location = new Location(myOtherCar.Location.Latitude + 2, myOtherCar.Location.Longitude - 1);
             }
 
             Console.ReadLine();
