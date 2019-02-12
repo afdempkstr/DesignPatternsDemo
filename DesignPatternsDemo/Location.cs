7d056e01a1a4cad6c724a7ef0f8a9cebb76f7240
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-12 19:51:26 +0200

    Added distance calculation

--- DesignPatternsDemo/Location.cs
+++ DesignPatternsDemo/Location.cs
@@ -40,14 +40,15 @@ namespace DesignPatternsDemo
                     Math.Abs(this.Longitude - other.Longitude) < 1e-6);
         }
 
-        //public double Distance(Location other)
-        //{
+        public double Distance(Location other)
+        {
+            var d1 = this.Latitude * (Math.PI / 180.0);
+            var num1 = this.Longitude * (Math.PI / 180.0);
+            var d2 = other.Latitude * (Math.PI / 180.0);
+            var num2 = other.Longitude * (Math.PI / 180.0) - num1;
+            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
 
-        //}
-
-        //private double EuclideanDistance(Location other)
-        //{
-            
-        //}
+            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+        }
     }
 }
