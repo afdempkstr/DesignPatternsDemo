2e68a3b9ddab3fa328d0780824ae2f4e08b0270a
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-12 18:44:56 +0200

    Add project files.

---
+++ DesignPatternsDemo/Location.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatternsDemo
+{
+    public class Location
+    {
+        public double Latitude { get; }
+
+        public double Longitude { get; }
+
+        public Location(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
---
+++ DesignPatternsDemo/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatternsDemo
+{
+    public class Vehicle
+    {
+        public string Id { get; }
+
+        public Location Location { get; set; }
+
+        public Vehicle(string id)
+        {
+            Id = id;
+        }
+    }
+}
